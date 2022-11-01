10450b993531bf4dd7c5f60c9299167aaaf7c35b
Ratheeshbabu5551 <Ratheeshbabu5551@gmail.com>
2022-11-01 19:12:55 +0530

    loops

---
+++ novemberpython/buildin methods and loops.py
@@ -0,0 +1,11 @@
+txt = "apple#banana#cherry#orange"
+x = txt.split("#",1)
+print(x)
+
+txt = "hello, my name is Ratheesh, I am 22 years old"
+x = txt.split("/     ")
+print(x)
+
+fruits = ["apple", "banana", "cherry","orange"]
+for x in fruits:
+  print(x)
