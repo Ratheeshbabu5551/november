666952ce07442d35152d17dd4cad265a14c61149
Ratheeshbabu5551 <Ratheeshbabu5551@gmail.com>
2022-11-01 13:14:34 +0530

    sliceing

--- novemberpython/november.py
+++ novemberpython/november.py
@@ -1,5 +1,4 @@
-#
-
+''''''
 str="Ratheesh babu"
 print(str)
 print(str[0])
@@ -8,3 +7,31 @@ print(str[:-1])
 print(str[::-1])
 print(str*5)
 print(str[1:])
+
+a,b,c =2,20.0,"ratheesh"
+print(a,b,c)
+print(a)
+print(c)
+
+x=["danny",1,34,40.2,2]
+y=[23,"ratheesh"]
+print(x[0:])
+print(x+y)
+print(x*2)
+print(x[::-1])
+''''''
+print(type(y))
+
+x =23
+y =4.6
+z = 4444
+w = 4
+print(type(y))
+print(type(w))
+
+x = int(1)
+y = int(2.7)
+z = int("3")
+print(x,y,z)
+
+
