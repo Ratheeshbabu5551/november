e7a9d02e9fca9abb99fcf1dad890b333c173a426
Ratheeshbabu5551 <Ratheeshbabu5551@gmail.com>
2022-11-02 11:39:06 +0530

    loops

--- novemberpython/buildin methods and loops.py
+++ novemberpython/buildin methods and loops.py
@@ -1,11 +1,34 @@
-txt = "apple#banana#cherry#orange"
-x = txt.split("#",1)
-print(x)
 
-txt = "hello, my name is Ratheesh, I am 22 years old"
-x = txt.split("/     ")
-print(x)
+a = 277
+b = 33
+c = 899
+if a > b and c > a:
+  print("Both conditions are True")
+"""
+i = 1
+while i < 9:
+  print(i)
+  i += 1
+"""
+i = 1
+while i < 6:
+  print(i)
+  if i == 6:
+    break
+  i += 1
 
-fruits = ["apple", "banana", "cherry","orange"]
-for x in fruits:
-  print(x)
+
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+
+
+list1 = ["Ram", "gopi", "Mohan"]
+list2 = [24, 56, 34]
+for x, (list1, list2) in enumerate(zip(list1, list2)):
+    print(x, list1, list2)
